201b5f105a9af4dac0e6f5dd595c8a77db89884c
wangliqiu2021 <wangliqiu2021@163.com>
2022-08-18 01:47:02 +0800

    Q

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,15 @@ mod template;
 pub use template::*;
 
 
+/// diff from dbg! macro : only for Debug build, omit stringify!(expression)
+#[macro_export]
+#[cfg(debug_assertions)]
+macro_rules! debug {
+    ($val:expr $(,)?) => {
+        match $val {
+            tmp => {
+                println!("[{}:{}]\n{:#?}", file!(), line!(), &tmp);
+            }
+        }
+    };
+}
--- src/template/ring.rs
+++ src/template/ring.rs
@@ -1,24 +1,28 @@
-use data_encoding::HEXUPPER;
 use ring::digest::{Context, Digest, SHA256};
-use std::fs::File;
-use std::io::{BufReader, Read, Write};
-use crate::ResultX;
+use std::io::{BufRead, BufReader, Read};
+use crate::{debug, ResultX};
+use ring::{digest, hmac, rand};
+use ring::hmac::Tag;
 
+/// SHA-256
+///
 /// for large content to read
-pub fn sha256<R: Read>(mut read: R) -> ResultX<Digest> {
+pub fn sha256<R: Read>(read: R) -> ResultX<Digest> {
     let mut cx = Context::new(&SHA256);
-    let mut buf = [0; 1024];
-
-    let mut read = BufReader::new(read );
-
-    read.read_exact();
+    let mut bufread = BufReader::new(read);
 
     loop {
-        let count = read.read(&mut buf)?;
-        if count == 0 {
+        let buf = bufread.fill_buf()?;
+        let len = buf.len();
+        if len == 0 {
             break;
         }
-        cx.update(&buf[..count]);
+
+        // `update` can be called many times until finish is called
+        cx.update(&buf[..len]);
+
+        // `consume` must be called to be paired with the `fill_buf`
+        bufread.consume(len);
     }
 
     Ok(
@@ -28,23 +32,39 @@ pub fn sha256<R: Read>(mut read: R) -> ResultX<Digest> {
 }
 
 
+
+/// HMAC: Hash-based Message Authentication Code
+///
+pub fn hmac(data: &[u8]) -> ResultX<Tag> {
+    let rng = rand::SystemRandom::new();
+    let raw_key: [u8; digest::SHA256_OUTPUT_LEN] = rand::generate(&rng)?.expose();
+    // generate i_pad_key and o_pad_key, then store them in Key's inner and outer Context
+    let key = hmac::Key::new(hmac::HMAC_SHA256, &raw_key);
+    let tag = hmac::sign(&key, data);
+
+    // calculates data by key and verifies whether the result equals tag
+    debug!(hmac::verify(&key, data, tag.as_ref())?);
+
+    Ok(tag)
+}
+
+
 #[cfg(test)]
 mod tests {
     use std::fs::File;
-    use std::io::BufReader;
     use data_encoding::HEXUPPER;
-    use crate::ResultX;
-    use crate::ring::sha256;
+    use crate::ring::{hmac, sha256};
 
     #[test]
-    fn main() -> ResultX<()> {
-        let path = "./src/lib.rs";
-
-        let read = BufReader::new(File::open(path)?);
-        let digest = sha256(read)?;
+    fn test_sha256() {
+        let file = File::open("./src/lib.rs").unwrap();
+        let digest = sha256(file).unwrap();
 
         println!("SHA-256 digest is {}", HEXUPPER.encode(digest.as_ref()));
+    }
 
-        Ok(())
+    #[test]
+    fn test_hmac() {
+        let _ = hmac("data".as_bytes()).unwrap();
     }
 }
